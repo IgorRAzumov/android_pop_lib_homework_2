72baa82b17200c279f5804fb58e383ceaec62368
Igor <1itcode1@gmail.com>
2018-05-28 22:03:42 +0300

    bug fix

--- app/src/main/java/geekbrains/ru/android_pop_lib_homework_2/presenters/TestPresenter.java
+++ app/src/main/java/geekbrains/ru/android_pop_lib_homework_2/presenters/TestPresenter.java
@@ -5,9 +5,10 @@ import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
 import geekbrains.ru.android_pop_lib_homework_2.activities.TestActivity;
+import geekbrains.ru.android_pop_lib_homework_2.views.TestView;
 
 @InjectViewState
-public class TestPresenter extends MvpPresenter<TestActivity> {
+public class TestPresenter extends MvpPresenter<TestView> {
 
     public void entryTextChange(CharSequence text) {
         getViewState().setText(text.toString());
